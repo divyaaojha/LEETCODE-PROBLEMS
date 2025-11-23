ca9b7ba0de87531398fb13067657995aed3b1654
Divya Ojha <divyaojha792@gmail.com>
2025-11-23 15:25:19 +0530

    Time: 5 ms (28.73%) | Memory: 43.5 MB (17.81%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,26 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int hindex =0;
         int n = citations.length;
-        for(int i =0; i < n; i++){
-           int paperwithatleastthiscite= n-i;
-           //since it is sorted in asc, so to its right all papers with equal or more citataions are mentioned
-            //i.e. n-i no of papers have greater than or equal to cites
-            
+        Arrays.sort(citations);
+        int hindex=0;
 
-            int currentcitationatleastHvalue = citations[i];
-            if(currentcitationatleastHvalue  >= paperwithatleastthiscite){
-                hindex = paperwithatleastthiscite;
-                //got answer
-
-                break;
+        for(int i =0; i<n; i++){
+            if(citations[i] >= (n-i)){
+                int temp = n-i;
+                hindex =Math.max(hindex, temp);
             }
-
-
-
         }
-          
         return hindex;
     }
 }
