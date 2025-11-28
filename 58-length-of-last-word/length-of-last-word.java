421f2f2aa34dd3f1fa7f89ef944a247ddc7e3491
Divya Ojha <divyaojha792@gmail.com>
2025-11-28 09:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (34.07%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length()-1;
+        int count=0;
+
+       while(n >= 0 && s.charAt(n) == ' '){
+        n =n-1;
+       }
+       while(n>= 0 && s.charAt(n) != ' '){
+        count++;
+        n--;
+       }
+
+
+       return count;
+    }
+}
