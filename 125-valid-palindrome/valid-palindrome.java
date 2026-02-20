2113a270ea9621db618be7bb6822318d2007f407
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-20 11:15:21 +0530

    Time: 2 ms (98.98%) | Memory: 44.6 MB (53.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,52 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                sb.append(Character.toLowerCase(c));
-            }
-        }
+      
+        
 
-        int left =0; int right = sb.length()-1;
+        int left =0; int right = s.length()-1;
         while(left < right){
 
-            if(sb.charAt(left)  !=sb.charAt(right)){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
                 return false;
             }
+
             left++;
             right--;
 
         }
-        return true;
+        return true; 
     }
+
+
+    //using another string- passed, optimised
+    //  StringBuilder sb = new StringBuilder();
+    //     for(char c : s.toCharArray()){
+    //         if(Character.isLetterOrDigit(c)){
+    //             sb.append(Character.toLowerCase(c));
+    //         }
+    //     }
+
+    //     int left =0; int right = sb.length()-1;
+    //     while(left < right){
+
+    //         if(sb.charAt(left)  !=sb.charAt(right)){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+
+    //     }
+    //     return true;
+
+
+        //*=------------------------------------------------
 
     // it doesnt ignore uppercase, and alphanumeric characters and spaces
     // private boolean recur(String s, int len){
