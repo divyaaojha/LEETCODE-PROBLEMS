0e0fb1d45f636c88ad1c281ff727c55b3f3b9c46
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 16:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 61.2 MB (86.27%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,19 +1,41 @@
 class Solution {
+       private void reverse(int[]arr, int l, int r){
+        while(l<r){
+        int temp = arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+
+        l++;
+        r--;
+        }
+        
+    }
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k%n;
-        int[] temp = new int[n];
-        for(int i =0; i<n; i++){
-            //move every step ahead by k
-            temp[(i+k) %n] = nums[i];
+        //this is not in-place solution, but works
+        // int n = nums.length;
+        // k = k%n;
+        // int[] temp = new int[n];
+        // for(int i =0; i<n; i++){
+        //     //move every step ahead by k
+        //     temp[(i+k) %n] = nums[i];
+
+        // }
+        // for(int i =0; i<n; i++){
+        //     //move every step ahead by k
+        //     nums[i]= temp[i];
+
+        // }
+    
+           int n = nums.length;
+           k = k%n;
+       
+            reverse(nums, 0, n-1);
+            reverse(nums, 0, k-1);
+            reverse(nums, k, n-1);
 
         }
-        for(int i =0; i<n; i++){
-            //move every step ahead by k
-            nums[i]= temp[i];
-
-        }
-
 
     }
-}
+ 
+
+
