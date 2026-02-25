e7d245bad72011dffa43de6b6036e0698824ce64
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 23:19:02 +0530

    Time: 23 ms (93.87%) | Memory: 49.2 MB (11.98%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,20 +1,36 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        // sliding window concept - i guess
-        int count =0;
-        for(int i =0; i<nums.length; i++){
-            int sum =0;
-            for(int j =i; j<nums.length; j++){
+        //optimised, hashmap + prefix sum = solution O(N)- tc, O(N)-sc
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int prefix=0;
+        int count=0;
+        for(int num: nums){
+            prefix+=num;
 
-                sum+=nums[j];
-
-                if(sum == k){
-                    count++;
-                    
-                }
-                
+            if(map.containsKey(prefix-k)){
+                count += map.get(prefix-k) ;
             }
+            map.put(prefix, map.getOrDefault(prefix,0)+1);
         }
-        return count;
+    return count;
+
+        //THIS WORKS BUT IS O(n2) tc:
+        // // sliding window concept - wont work, as it only works for positive arrays, 
+        // int count =0;
+        // for(int i =0; i<nums.length; i++){
+        //     int sum =0;
+        //     for(int j =i; j<nums.length; j++){
+
+        //         sum+=nums[j];
+
+        //         if(sum == k){
+        //             count++;
+                    
+        //         }
+                
+        //     }
+        // }
+        // return count;
     }
 }
