92d65448b8a95e5273e0f4d2ce4954b460019bc9
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 14:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (65.19%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -12,22 +12,6 @@ for(int i =0; i<nums.length; i++){
        return true;
 
 
-        //using formula, finding pivot, then *************************************
-    //    int max = Integer.MAX_VALUE;
-    //    int index=0;
-    //    //FIND MIN ELE- ITS INDEX IS PIVOT, AND THEN USE IT IN THE FORMULA, PIVOT= X
-    //    for(int i =0; i<nums.length; i++){
-    //     if(nums[i] <max){
-    //         max = nums[i];
-    //         index =i;
-    //     }
-    //    }
-
-    //    for(int i =1; i<nums.length; i++){
-    //     if( nums[(i+index) % nums.length] < nums[(i-1+index) % nums.length] ){
-    //         return false;
-    //     }
-    //    }
-    //    return true;
+      
     }
 }
