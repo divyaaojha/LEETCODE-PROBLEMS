39e66f577d3ac2773d05af6a63f8ddce1f9d14e4
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 19:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (83.35%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,21 +1,35 @@
 class Solution {
     public int missingNumber(int[] nums) {
 
-        //using treeset
-        int n = nums.length;
-        TreeSet <Integer> keyset = new TreeSet<>();
-       
-        for(int i = 0; i< nums.length; i++){
-            keyset.add(nums[i]);
-        }
+        //using treeset t.c 0(N) , sc 0(N), 
         
-        for(int i =0; i<=n ; i++){
-            if(! keyset.contains(i) ){
-                return i;
-            }
+        // int n = nums.length;
+        // TreeSet <Integer> keyset = new TreeSet<>();
+       
+        // for(int i = 0; i< nums.length; i++){
+        //     keyset.add(nums[i]);
+        // }
+        
+        // for(int i =0; i<=n ; i++){
+        //     if(! keyset.contains(i) ){
+        //         return i;
+        //     }
             
+        // }
+        // return -1;
+
+
+        //i need to optimise and make it sc 0(1) & tc 0(N)
+
+        int n = nums.length;
+        int sumcalculated =0;
+        for(int num: nums){
+            sumcalculated += num;
         }
-        return -1;
+
+        int totalsum = n*(n+1)/2;
+
+        return (totalsum-sumcalculated);
         
     }
 }
