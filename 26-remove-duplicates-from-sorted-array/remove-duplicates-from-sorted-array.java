ac8b1e3c17910c697c8526dbe40df5a41fb4de40
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 15:23:34 +0530

    Time: 10 ms (3.84%) | Memory: 47.3 MB (7.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -11,6 +11,20 @@ class Solution {
          Arrays.sort(nums);
          return count;
 
+        //USING HASHMAP
+        //         int count=1;
+        // for(int i =1; i<nums.length; i++){
+        //     int j =i;
+        //     while(j<nums.length && nums[i-1]==nums[j]){
+        //         nums[j] = Integer.MAX_VALUE;
+        //         j++;
+        //     }
+        //     if(nums[i]!= Integer.MAX_VALUE){
+        //         count++;
+        //     }
+        // }
+        // Arrays.sort(nums);
+        // return count;
 
 
         // int count=1;
