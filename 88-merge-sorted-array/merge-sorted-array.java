86dcba2659bcc457ba4da0963eda638452afe1cd
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-25 18:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (43.08%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,40 +1,42 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
- int k =0;                              
-for(int i =m; i<m+n; i++ ){
-nums1[i]=nums2[k++];
-}
-Arrays.sort(nums1);
+// // O((m+n)log(m+n)) solution - we want next o(m+n) solution
+//  int k =0;                              
+// for(int i =m; i<m+n; i++ ){
+// nums1[i]=nums2[k++];
+// }
+// Arrays.sort(nums1);
 
-
-// int[] num =new int[m+n];
-// int index =0;
-//  int i =0; int j =0;
-// while(i<m && j<n ){
+//******************************************************************************************************** */
+//o(m+n) solution
+int[] num =new int[m+n];
+int index =0;
+ int i =0; int j =0;
+while(i<m && j<n ){
    
     
-//     if(nums2[j] <= nums1[i]){
-//         num[index]=nums2[j];
-//          j++;
-//     }
-//     else{
-//         num[index]=nums1[i];
-//          i++;
-//     }
-//     index++;
+    if(nums2[j] <= nums1[i]){
+        num[index]=nums2[j];
+         j++;
+    }
+    else{
+        num[index]=nums1[i];
+         i++;
+    }
+    index++;
     
-// }
+}
 
-// while(i<m){
-//     num[index++]= nums1[i++];
-// }
-// while(j<n){
-//     num[index++]= nums2[j++];
-// }
+while(i<m){
+    num[index++]= nums1[i++];
+}
+while(j<n){
+    num[index++]= nums2[j++];
+}
 
-// for(int k =0; k< num.length ; k++){
-//     nums1[k]= num[k];
-// }  
+for(int k =0; k< num.length ; k++){
+    nums1[k]= num[k];
+}  
 
     }
 }
