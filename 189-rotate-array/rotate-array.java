6e67f99eaee5176678cd8a74da5a4686b700b9a8
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-26 17:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 61.2 MB (71.53%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,41 +1,24 @@
 class Solution {
-       private void reverse(int[]arr, int l, int r){
-        while(l<r){
-        int temp = arr[l];
-        arr[l]=arr[r];
-        arr[r]=temp;
-
-        l++;
-        r--;
+    void reverse(int[] arr, int a, int b){
+        while(a<b){
+            int temp = arr[a];
+            arr[a]= arr[b];
+            arr[b]= temp;
+            a++;
+            b--;
         }
-        
     }
     public void rotate(int[] nums, int k) {
-        //this is not in-place solution, but works
-        // int n = nums.length;
-        // k = k%n;
-        // int[] temp = new int[n];
-        // for(int i =0; i<n; i++){
-        //     //move every step ahead by k
-        //     temp[(i+k) %n] = nums[i];
+       // rotate to right by k steps, without extra space
+       //reverse whole array
+       // reverse first k then
+       // reverse last n-k then
 
-        // }
-        // for(int i =0; i<n; i++){
-        //     //move every step ahead by k
-        //     nums[i]= temp[i];
-
-        // }
-    
-           int n = nums.length;
-           k = k%n;
-       
-            reverse(nums, 0, n-1);
-            reverse(nums, 0, k-1);
-            reverse(nums, k, n-1);
-
-        }
+       int n = nums.length;
+       k = k%n;
+       reverse(nums, 0, n-1);
+       reverse(nums, 0, k-1);
+       reverse(nums, k, n-1);
 
     }
- 
-
-
+}
