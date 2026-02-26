7a936ac432b3fe79caf994a6a8ca83c91c092321
Divyaa Ojha <divyaojha792@gmail.com>
2026-02-26 11:40:58 +0530

    Time: 3 ms (60.75%) | Memory: 52.8 MB (9.32%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,32 +1,126 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-        // KEEP COUNTING CURRENT 1'S, AND RESET COUNT TO 0 WHEN 0 ENCOUNTERED
-      int n = nums.length;
-      int finalcount =0;
-      int count=0;
-      for(int i =0; i<n; i++){
-        if(nums[i]==0) count=0;
-        else {count++;
-        finalcount= Math.max(finalcount, count);}
-      }
+int maxcount =0;
+int count=0;
+
+for(int i =0; i<nums.length; i++){
+    if(nums[i]==1){
+        count++;
+        maxcount= Math.max(maxcount, count);
+
+    }
+    if(nums[i]==0){
+        count=0;
+    }
+}
+
+return maxcount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     // KEEP COUNTING CURRENT 1'S, AND RESET COUNT TO 0 WHEN 0 ENCOUNTERED
+    //   int n = nums.length;
+    //   int finalcount =0;
+    //   int count=0;
+    //   for(int i =0; i<n; i++){
+    //     if(nums[i]==0) count=0;
+    //     else {count++;
+    //     finalcount= Math.max(finalcount, count);}
+    //   }
      
-      return finalcount;
+    //   return finalcount;
 
 
-      //TLE***********************************************************
-        // int n = nums.length;
-        // int finalcount =0;
-        // int j =0;
-        // while(j <n){
-        //     int count=0;
-        //     if(nums[j]==1)continue;
-        //     while(nums[j]==0){
-        //         count ++;
+    //   //TLE***********************************************************
+    //     // int n = nums.length;
+    //     // int finalcount =0;
+    //     // int j =0;
+    //     // while(j <n){
+    //     //     int count=0;
+    //     //     if(nums[j]==1)continue;
+    //     //     while(nums[j]==0){
+    //     //         count ++;
                  
-        //     }
-        //     finalcount = Math.max(finalcount, count);
-        // }
+    //     //     }
+    //     //     finalcount = Math.max(finalcount, count);
+    //     // }
 
-        // return finalcount;
+    //     // return finalcount;
     }
 }
