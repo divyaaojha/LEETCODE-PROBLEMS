46763cc27251f0731d47e80d2b288010b66c4435
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-06 11:08:46 +0530

    Time: 10 ms (48.63%) | Memory: 58.3 MB (5.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,16 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> freqmap = new HashMap<>();
+        Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
-
-        for(int i =0; i<n; i++){
-            freqmap.put(nums[i], freqmap.getOrDefault(nums[i], 0)+1);
-            if(freqmap.get(nums[i]) > (n/3) && !ans.contains(nums[i])){
-                ans.add(nums[i]);
-            }
+        int count=0;
+       int i =0; int j =0;
+       while(j<n){
+        if(nums[i]==nums[j]){
+            j++;
+            count++;
+            if(count> (n/3) && !ans.contains(nums[i]) ){
+                    ans.add(nums[i]);
+                }
         }
-        return ans;
+        if( j <n && nums[i] != nums[j]){
+            i =j;
+            count=0;
+        }
+       }
+
+return ans;
+
+        /************************************************************************************************************** */
+        // int n = nums.length;
+        // HashMap<Integer, Integer> freqmap = new HashMap<>();
+        // List<Integer> ans = new ArrayList<>();
+
+        // for(int i =0; i<n; i++){
+        //     freqmap.put(nums[i], freqmap.getOrDefault(nums[i], 0)+1);
+        //     if(freqmap.get(nums[i]) > (n/3) && !ans.contains(nums[i])){
+        //         ans.add(nums[i]);
+        //     }
+        // }
+        // return ans;
+        //**************************************************************************************** */
         // //brute force, tc= n^2, sc = 0(1)
         // int n = nums.length;
         // int count=0;
