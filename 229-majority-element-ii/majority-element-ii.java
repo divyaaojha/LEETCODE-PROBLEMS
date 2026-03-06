b7439a5ffe89e2f926df0857a6f302f742fea536
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-06 10:58:18 +0530

    Time: 20 ms (8.98%) | Memory: 52.1 MB (82.00%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,25 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-         int count=0;
+        HashMap<Integer, Integer> freqmap = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
+
         for(int i =0; i<n; i++){
-         
-           
-            for(int j =i; j<n; j++){
-                if(nums[i]==nums[j]){
-                    count++;    
-                 }
-
-                }
-                if(count> (n/3) && !ans.contains(nums[i]) ){
-                    ans.add(nums[i]);
-                }
-                count=0;
-
+            freqmap.put(nums[i], freqmap.getOrDefault(nums[i], 0)+1);
+            if(freqmap.get(nums[i]) > (n/3) && !ans.contains(nums[i])){
+                ans.add(nums[i]);
             }
-            
-        
+        }
         return ans;
+        // //brute force, tc= n^2, sc = 0(1)
+        // int n = nums.length;
+        // int count=0;
+        // List<Integer> ans = new ArrayList<>();
+        // for(int i =0; i<n; i++){
+        //     for(int j =i; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;    
+        //          }
+        //         }
+
+        //         if(count> (n/3) && !ans.contains(nums[i]) ){
+        //             ans.add(nums[i]);
+        //         }
+        //         count=0;
+        //     }
+        // return ans;
     }
 }
