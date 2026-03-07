950a6612e108216410e112f156f2c786510e247c
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-07 09:10:05 +0530

    Time: 56 ms (63.39%) | Memory: 115.2 MB (32.24%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
@@ -1,23 +1,45 @@
 class Solution {
     public long beautifulSubarrays(int[] nums) {
-        // this is basically: count subbarray with XOR =0;
-     
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
-        long count =0;
-        int prefixxor= 0; // // a ^ 0 = a ;
 
-        for(int num:nums){
-            prefixxor ^= num;
-            
-            //we can simply write prefixxor instead of prefixxor ^0, cuz they are same, its written for clarity
-            if(map.containsKey(prefixxor^0)){
-                count += map.get(prefixxor ^0);
+        int n = nums.length;
+        int prexor=0;
+        long count=0;
+        HashMap<Integer, Integer> map= new HashMap<>();
+        //storing prefix xor and its count in map
+        map.put(0,1);
+
+        for(int i=0; i<n; i++){
+            prexor ^= nums[i];
+            if(map.containsKey(prexor)){
+                count+= map.get(prexor);
             }
 
-            map.put(prefixxor, map.getOrDefault(prefixxor, 0)+1);
+            map.put(prexor, map.getOrDefault(prexor, 0)+1);
         }
 
+
         return count;
+
+
+
+        // // this is basically: count subbarray with XOR =0;
+     
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(0,1);
+        // long count =0; // it has to be long as for very large array it will exceed integer.max_value;
+        // int prefixxor= 0; // // a ^ 0 = a ;
+
+        // for(int num:nums){
+        //     prefixxor ^= num;
+            
+        //     //we can simply write prefixxor instead of prefixxor ^0, cuz they are same, its written for clarity
+        //     if(map.containsKey(prefixxor^0)){
+        //         count += map.get(prefixxor ^0);
+        //     }
+
+        //     map.put(prefixxor, map.getOrDefault(prefixxor, 0)+1);
+        // }
+
+        // return count;
     }
 }
