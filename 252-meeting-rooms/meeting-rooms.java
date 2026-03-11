c217067eaada51e7babfd300d19d85d4addf9fbf
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-11 12:22:16 +0530

    Time: 7 ms (26.48%) | Memory: 47.9 MB (98.24%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
+        //The key idea is checking if any meetings overlap.
+        // If two meetings overlap, the person cannot attend both → return false.
+        // If no overlaps exist, return true.
 
        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
        for(int i =1; i< intervals.length; i++){
