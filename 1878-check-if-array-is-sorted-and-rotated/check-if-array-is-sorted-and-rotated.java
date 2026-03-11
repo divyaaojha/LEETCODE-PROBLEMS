7f23e30d95f354951749b16e25180b5fa2a4381d
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-11 09:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (43.36%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,39 @@
 class Solution {
     public boolean check(int[] nums) {
-int count=0;
-for(int i =0; i<nums.length; i++){
-        if( nums[i % nums.length] > nums[(i+1) % nums.length] ){
-            count++;
-            if (count >1){
-                return false;
-            }
-        }
+// count dips- only 1 dip in rotated sorted array where, nums[i]> nums[i+1] in sorted array
+int x=0;
+int n = nums.length;
+int count =0;
+for(int i =0; i<n; i++){
+    if(nums[i %n]> nums[(i+1) %n]){
+       count++;
+       if(count>1){
+        return false;
        }
-       return true;
+       
+    }
+}
+
+
+return true;
+
+
+
+
+
+
+
+
+// int count=0;
+// for(int i =0; i<nums.length; i++){
+    //     if( nums[i % nums.length] > nums[(i+1) % nums.length] ){
+    //         count++;
+    //         if (count >1){
+    //             return false;
+    //         }
+    //     }
+    //    }
+    //    return true;
 
 
       
