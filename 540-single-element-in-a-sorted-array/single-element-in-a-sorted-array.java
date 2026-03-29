99be9b71bbececd726d2ec8d563cf8dbebba421a
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-29 08:34:56 +0530

    Time: 1 ms (26.17%) | Memory: 52.8 MB (84.86%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,31 +1,10 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n= nums.length;
-        int low =0; int high = n-1;
-        //doing binary search, prev pairs to single element start at even index, so we ll make sure our mid is even 
-        while(low<high){
-            int mid = low + (high-low)/2;
-
-            //mid was odd, so we did mid--, so we dnt miss the starting index of the pairs
-            if(mid % 2 == 1){
-                mid--;
-            } 
-
-            if(nums[mid] == nums[mid+1]){
-            // we made sure our mid ws even, and if starting index of pair is even - 
-            //this pattern was followed, means our single lies ahead of mid+1;
-            low=mid+2;
-            //we are changing low pointer only when equal pairs, the single is after this pair, 
-            //so low is mid+2; this way it can have answer index
-            }
-            // else, either mid is the single, or leftward lies the single.
-            else{
-                high =mid;
-            }
-
+     
+        int xor=0;
+        for(int n :nums){
+            xor ^=n;
         }
-
-        return nums[low];
-    
+        return xor;
     }
 }
