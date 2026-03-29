96996069005e7c5ee9ec37f8d16cba5c7a3871a1
Divyaa Ojha <divyaojha792@gmail.com>
2026-03-29 14:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (59.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,26 +1,72 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left =0;
-        int right = nums.length-1;
-        int index =0;
-        
-        while(left<= right){
-            int mid = left + (right-left)/2;
-           if(nums[mid] == target ) {
-            index =mid;
-            return mid;
-           }
+        int n = nums.length;
+        int l =0; int r =n-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
 
-           else if (nums[mid]< target){
-            left= mid+1;
-            index=left;
-            
-           }
-           else{
-            right = mid-1;
-           
-           }
+            if(nums[mid]>= target){
+                r= mid-1;
+            }
+
+            else{
+                l = mid+1;
+            }
         }
-         return index;
+
+        return l;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int left =0;
+        // int right = nums.length-1;
+        // int index =0;
+        
+        // while(left<= right){
+        //     int mid = left + (right-left)/2;
+        //    if(nums[mid] == target ) {
+        //     index =mid;
+        //     return mid;
+        //    }
+
+        //    else if (nums[mid]< target){
+        //     left= mid+1;
+        //     index=left;
+            
+        //    }
+        //    else{
+        //     right = mid-1;
+           
+        //    }
+        // }
+        //  return index;
     }
 }
