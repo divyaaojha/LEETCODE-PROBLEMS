59dc7d1151bcddb8a63662fb4df974869eba7a51
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-19 21:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (91.33%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,29 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //this is a row wise sorted and column wise sorted matrix,
-        //first integer of each row is greater than the last integer of the previous row- this means
-        //column wise sorted matrix
-        // so the approach is basically, when we choose the
-        // -  upper rightmost corner-> means, to its left smaller element, below it larger element
-        // - lower leftmost corner->means, to its right larger elements, 
-        // in other 2 corners, we have either smaller choices both sides, or larger choices both sides, to choose from
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        int row=matrix.length;
-        int col=matrix[0].length;
+        int totalelements = rows *cols;
+        int low = 0; 
+        int high = totalelements -1 ;
 
-        int i =0; int j = col-1;
-        //i - row pointer, j col pointer
-        while(i<row && j>=0){
-            if(matrix[i][j]==target){
+
+        // since rows are sorted, only
+        //But here the property is: First element of a row > last element of previous row
+        // this property basically means  - the whole array is sorted , and on flateening it will become one sorted 1d array
+        // so we ll do exactly that- and flatten the array,
+        while( low <= high){
+            int mid = low + (high -low)/2;
+            //mapping logic 2d -> 1d
+            int r = mid/cols;
+            int c = mid % cols;
+            if(matrix[r][c] == target){
                 return true;
             }
-            else if(matrix[i][j]>target){
-                 j--;
+            else if(  target > matrix[r][c] ){
+                low = mid + 1;
+
             }
             else{
-                i++;
+                high = mid-1;
             }
         }
-    return false;
+        return false;
     }
 }
