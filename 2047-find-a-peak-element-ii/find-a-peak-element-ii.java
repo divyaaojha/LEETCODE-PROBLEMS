c6a30b9100e2c89317f6c507a32559cd993dc964
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-20 21:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 116.3 MB (27.13%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -2,20 +2,123 @@ class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int row = mat.length;
         int col =mat[0].length;
-        for(int i =0; i<row; i++){
-            for(int j =0; j<col; j++){
-                
-                int curr = mat[i][j];
-                // we ll check the boundary only if they exist
-                int top = (i ==0) ? -1 : mat[i-1][j];
-                int left = (j ==0) ? -1: mat[i][j-1];
-                int right = (j == (col-1)) ? -1:mat[i][j+1] ;
-                int bottom = (i== (row-1)) ? -1 : mat[i+1][j];
-                if(curr >top && curr >bottom && curr >left && curr > right ) return new int[]{i,j};
 
+        //we are fixing col by finding col max, and so we will compare with left and right- so left and right boundary are col boundary
+        int low =0;
+        int high = col-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            //mid is column, as we are doing binary search on column- so now we selected the col - 
+            //now find the row which has max element in this col
+
+            int maxi=-1;
+            int maxrow =0;
+            for(int i =0; i< row; i++){
+                if(mat[i][mid] > maxi){ 
+                maxi = mat[i][mid]; 
+                maxrow=i;
+                }
             }
+            // row = maxrow, col =mid
+            //check left and right now, and move direction also left/right only
+            int left = (mid > 0) ? mat[maxrow][mid - 1] : -1;
+            int right = (mid < col - 1) ? mat[maxrow][mid + 1] : -1;
+            int curr = mat[maxrow][mid];
+            if( curr > left && curr > right){
+                return new int[]{maxrow,mid};
+            }
+            else if( left > curr ){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+
+
         }
-        return new int[]{0,0};
+
+       
+      
+    
+    return new int []{0,0};
+
+    //57 / 59 testcases passed---- read approach, did it myself**********************
+    //this is luck by chance - be happy cuz you tried, dont be happy cus it passed test cases, 
+    //because you have no idea why the rest 2 test cases failed why your code didnt work- you need to debug*****
+    //these are the important question one has to ask / seek
+
+
+        // int row = mat.length;
+        // int col =mat[0].length;
+       
+        // for(int j =0; j< col; j++){
+        //     // fixed the row 
+           
+     
+        //         int low = 0;
+        //         int high = row-1;
+
+        //         while( low <= high){
+        //         int mid = low + (high -low)/2;
+        //         int top = (mid ==0) ? -1 : mat[mid-1][j];
+        //         int bottom = (mid== (row-1)) ? -1 : mat[mid+1][j];
+        //          int curr = mat[mid][j];
+        //         if(curr > top && curr > bottom ){
+        //         int left = (j ==0) ? -1: mat[mid][j-1];
+        //         int right = (j == (col-1)) ? -1:mat[mid][j+1] ;
+        //          if( curr > left && curr >right ){
+        //             return new int []{mid,j};
+        //          }
+
+        //         }
+
+        //         if(top> curr){
+        //             high =mid-1;
+        //         }
+        //         else{
+        //             low = mid+1;
+        //         }
+        //     }
+        // }
+        // return new int []{0,0};
+// Step 1: Recognize Pattern
+
+// This is similar to:
+// 👉 1D Peak Element problem (Leetcode 162)
+// Where we use binary search on array
+
+// Step 2: Extend 1D → 2D
+// We can't binary search both dimensions.
+// So we:
+// 👉 Fix one dimension
+// 👉 Binary search on the other
+//************************************************************************************************************ */
+
+
+
+
+
+
+
+        // BRUTE FORCE - PASSED ALL CASES- STANDARD*******************************************************
+        //concept -  ******** we ll check the boundary only if they exist ********
+        // int row = mat.length;
+        // int col =mat[0].length;
+        // for(int i =0; i<row; i++){
+        //     for(int j =0; j<col; j++){
+                
+        //         int curr = mat[i][j];
+        //         // we ll check the boundary only if they exist
+        //         int top = (i ==0) ? -1 : mat[i-1][j];
+        //         int left = (j ==0) ? -1: mat[i][j-1];
+        //         int right = (j == (col-1)) ? -1:mat[i][j+1] ;
+        //         int bottom = (i== (row-1)) ? -1 : mat[i+1][j];
+        //         if(curr >top && curr >bottom && curr >left && curr > right ) return new int[]{i,j};
+
+        //     }
+        // }
+        // return new int[]{0,0};
 
 
         //***********************************FIRST TRY************************************************* */
