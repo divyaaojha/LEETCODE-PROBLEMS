d14b26096c5e85f4aa75db2a8ae793befcbc6885
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-23 07:43:21 +0530

    Time: 6 ms (82.01%) | Memory: 44.8 MB (28.73%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,71 +1,19 @@
 class Solution {
     public String reverseWords(String s) {
+        String str = s.trim().replaceAll("\\s+", " "); // all leading and trailing and extra spaces removed 
+        String words []= str.split(" "); // every word separated now
+        StringBuilder sb = new StringBuilder();
+        for(int i= words.length -1; i>=0; i--){
+            sb.append(words[i]);
+            if(i!=0){
+                sb.append(" ");
 
-//using stack now
+            }
+        }
+        return sb.toString();
 
-Stack<String>stack = new Stack<>();
-String[]words= s.trim().split("\\s+");
-StringBuilder sb= new StringBuilder();
-for(String word: words){
-    stack.push(word);
-}
-while(!stack.isEmpty()){
-    String temp = stack.pop();
-    sb.append(temp);
-  if(!stack.isEmpty()){
-    sb.append(" ");
-  }
-}
-
-return sb.toString();
-//accepted solution *******************************************************************************************
-//  String[] words= s.trim().split("\\s+");
-//  //bs now reverse the words position wise , and we have the ans, after joing them into a string
-//  int left=0; int right = words.length -1;
-//  while(left<right){
-//     String temp = words[left];
-//     words[left]=words[right];
-//     words[right]=temp;
-//     left++;
-//     right--;
-//  }
-
-// return String.join(" ",words);
-    
- 
+        
 
 
-
-
-    //this doesnt work for multiple spaces in between words- rest works well;
-    //****************************************************************************** */
-    //     public static void reverse( char[] chararr, int low, int high){
-    //     //char array can be modified - not returning anything just modifying it
-    //     while(low<high){
-    //         char temp= chararr[low];
-    //         chararr[low]= chararr[high];
-    //         chararr[high]=temp;
-    //         low++;
-    //         high--;
-    //     }
-    // }
-    // public String reverseWords(String s) {
-    //     s = new String(s.trim());
-    //     char[] chararr= s.toCharArray();
-    //       int n = s.length();
-    //       int start=0; 
-    //       for(int end=0; end<n+1; end++){
-            
-    //         if(chararr[end]==' ') {
-    //             reverse(chararr, start, end-1);
-    //             start=end+1;
-    //         }
-    //       }
-    //       reverse(chararr, start, n-1);
-    //       reverse(chararr,0, n-1);
-
-    // return new String(chararr);
-
-    // }
     }
 }
