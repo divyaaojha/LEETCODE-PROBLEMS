72776a18914f9eb492c0c998c9ed1717ac2ff98e
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-24 12:49:55 +0530

    Time: 1 ms (99.87%) | Memory: 46.9 MB (75.72%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -30,24 +30,34 @@ class Solution {
         //****************************************************************************************** */
         //substring must start from very beginning of num, and end at the first odd num we encounter- 
         //as a number being odd only depends on the last digit, and it must be largest so it must start from very beginning
+        // int n =num.length();
+        // String ans ="";
+        // //both pointer start from same position
+        // int start=0;
+        // int end =0;
+        // while( end < n){
+        //     int digit = num.charAt(end) - '0';
+        //     //if current rightmost dig is odd - store as answer and continue
+        //     if(digit % 2 != 0 ){
+        //         ans = num.substring(start, end+1);
+        //     }
+        //     end++;
 
+        // }
+        // return ans;
+
+        //********************************************************************************************************* */
+        // also if i start finding the largest number from right most side, so first valid odd no is the answer itself, -more optimsed
         int n =num.length();
-        
         String ans ="";
-        //both pointer start from same position
-        int start=0;
-        int end =0;
-        while( end < n){
-            int digit = num.charAt(end) - '0';
-            //if current rightmost dig is odd - store as answer and continue
-            if(digit % 2 != 0 ){
-                ans = num.substring(start, end+1);
+        for( int i = n-1; i>=0; i--){
+            if((num.charAt(i) -'0' ) % 2 != 0){
+                ans = num.substring(0, i+1);
+                return ans;
             }
-            end++;
-
         }
+
         return ans;
-        
 
     }
 }
