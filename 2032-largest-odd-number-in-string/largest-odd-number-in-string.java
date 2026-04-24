e5bda77eac8d54aec7725833f8dc70a28c609218
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-24 12:32:38 +0530

    Time: 774 ms (5.39%) | Memory: 49 MB (9.91%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        
+        // ************************** trying to just pass the given test cases- 105/196 test cases passed ****************************************
+        //this doesnt handle cases for middle substrings : eg -> 123456= it doesnt evaluate, 23,2345,45 
+
+        // int x = Integer.parseInt(num);
+        // if(x%2 !=0)return String.valueOf(x);
+        // int rem =x % 10;
+        // x=x/10;
+        //  int ans=-1;
+        // if (rem % 2 != 0){
+        //     ans = rem;
+        // }
+        // while(x!=0){
+        //    if(x % 2 !=0){
+        //     //x is odd
+        //     if(x> ans){
+        //         ans = x;
+        //     }
+        //    }
+        //    rem = x%10;
+        //    x =x/10;
+        // }
+        // if(ans != -1){
+        //     return String.valueOf(ans);
+        // }
+        // return "";
+
+        //****************************************************************************************** */
+        //substring must start from very beginning of num, and end at the first odd num we encounter- 
+        //as a number being odd only depends on the last digit, and it must be largest so it must start from very beginning
+
+        int n =num.length();
+        
+        String ans ="";
+        //both pointer start from same position
+        int start=0;
+        int end =0;
+        while( end < n){
+            int digit = num.charAt(end) - '0';
+            //if current rightmost dig is odd - store as answer and continue
+            if(digit % 2 != 0 ){
+                ans = num.substring(start, end+1);
+            }
+            end++;
+
+        }
+        return ans;
+        
+
+    }
+}
