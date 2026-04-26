c12b5b5ba2348a569f202095da9a590d32bdd0b9
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-26 09:41:11 +0530

    Time: 19 ms (18.47%) | Memory: 44.3 MB (11.03%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,65 +1,54 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-       //try using hashmap- to map
-       HashMap<Character, Character> map = new HashMap<>();
-
+    
        int ssize = s.length();
        int tsize = t.length();
        if(tsize != ssize){
         return false;
        } 
 
-        //right now my loop isnt detecting, if all elemenst of s are different, then it isnt able to work as expected, lest see how we can fix this.
-        //let me add a check to count if count of different element , return false
-
-        //lets use freq array
-        int[]freqs= new int[200];
-        int[] freqt = new int[200];
         
-        for(int i =0; i< ssize; i++){
-            char ss = s.charAt(i);
-            char tt = t.charAt(i);
-            //comparing before putting value- so prev operations get checked
-            if(freqs[ss]!= freqt[tt]){
-                return false;
-            }
-            freqs[ss]=i+1;
-            freqt[tt]=i+1;
-            
-
-            
-        }
-       
-        return true;
+        // //lets use freq array
+        // int[]freqs= new int[200];
+        // int[] freqt = new int[200];
+        
+        // for(int i =0; i< ssize; i++){
+        //     char ss = s.charAt(i);
+        //     char tt = t.charAt(i);
+        //     //comparing before putting value- so prev operations get checked
+        //     if(freqs[ss]!= freqt[tt]){
+        //         return false;
+        //     }
+        //     freqs[ss]=i+1;
+        //     freqt[tt]=i+1;
+        // }
+        // return true;
 
 //sol 2
-    //    for(int i = 0; i<ssize; i++){
+ //try using hashmap- to map
+ //right now my loop isnt detecting, if all elemenst of s are different, then it isnt able to work as expected, lest see how we can fix this.
+//let me add a check to count if count of different element , return false
+
+HashMap<Character, Character> map1 = new HashMap<>();
+Set<Character> sset= new HashSet<>();
+Set<Character> tset= new HashSet<>();
 
 
-    //     if(map.containsKey(s.charAt(i)) ){
-           
-    //         if(map.get(s.charAt(i)) != t.charAt(i)){
-    //             return false;
-    //         }
-    //     }
-          
-    //      else {
-    //         map.put(s.charAt(i), t.charAt(i));
-    //     }
-      
-        
+       for(int i = 0; i<ssize; i++){
+        sset.add(s.charAt(i));
+        tset.add(t.charAt(i));
+        if(map1.containsKey(s.charAt(i)) ){
+            if(map1.get(s.charAt(i)) != t.charAt(i)){
+                return false;
+            }
+        }
+         else {
+            map1.put(s.charAt(i), t.charAt(i));
+        }
        
-    //    }
-    //    for(int i=0; i<tsize; i++){
-    //     if(map.containsKey(t.charAt(i))){
-    //         if(map.get(t.charAt(i))!=s.charAt(i)){
-    //             return false;
-    //         }
-    //     }
-    //     else{
-    //         map.put(t.charAt(i), s.charAt(i));
-    //     }
-    //    }
-    //    return true;
+       }
+       if(sset.size() != tset.size())return false;
+    
+       return true;
     }
 }
