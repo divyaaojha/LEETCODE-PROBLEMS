1c6267bf8c731ae3b637e4a9ad4a5af5567043b8
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-26 11:04:37 +0530

    Time: 4 ms (82.37%) | Memory: 46.5 MB (33.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,49 +1,10 @@
 class Solution {
-        static final int CHAR= 256;
     public boolean isAnagram(String s, String t) {
-
-
-// 0(n) solution
-          int n = s.length();
-        int m = t.length();
-        if(m!=n)return false;
-        int [] count = new int[CHAR];
-        
-            for(int j =0; j<n ; j++){ // both same length as
-                count[t.charAt(j)]++;
-                count[s.charAt(j)]--;
-           
-        }
-
-        for(int i =0; i<CHAR; i++){
-            if(count[i]!= 0){
-                return false;
-            }
-        }
-
-        return true;
-
-
-
-
-        //  O(n^2) solution - 
-        // int n = s.length();
-        // int m = t.length();
-        // if(m!=n)return false;
-        // int [] count = new int[CHAR];
-        // for(int i =0; i<=n-m ; i++){
-        //     for(int j =0; j<m ; j++){
-        //         count[t.charAt(j)]++;
-        //         count[s.charAt(i+j)]--;
-        //     }
-        // }
-
-        // for(int i =0; i<CHAR; i++){
-        //     if(count[i]!= 0){
-        //         return false;
-        //     }
-        // }
-
-        // return true;
+        char[]cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+        if(Arrays.equals(cs,ct))return true;
+        return false;
     }
 }
