cefad6c133ff5d72f012596652e552b0871f1e28
Divyaa Ojha <divyaojha792@gmail.com>
2026-04-26 10:23:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (74.38%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,7 +1,59 @@
 class Solution {
+   
+    
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()) return false;
-        return (s+s).indexOf(goal)>=0;
+        if(s.length() != goal.length())return false;
+        String str = s+s;
+        if(str.contains(goal)){
+            return true;
+        }
+        return false;
 
+
+
+        //only half correct- not working for aaab-> abaa kinda case 42/62 test cases 
+        
+        // if(s.length() != goal.length())return false;
+        // int n = s.length();
+        // char a []= s.toCharArray();
+        // char b []=goal.toCharArray();
+        // int k =0;
+        // int index=0;
+        // for(int i =0; i<n ; i++){
+        //     if(a[i] != b[index])k++;
+        //     else{
+        //         break;
+        //     }
+        // }
+        // for(int  i=0; i<n; i++){
+        //     if(a[(i+k )%n] != b[i])return false;
+        // }
+
+        // return true;
+
+
+        // convert both in char array, and then check rotation in array*****
+        //Just trying
+
+        // int i =0, j =0;
+        // if(s.length() != goal.length())return false;
+        // int index =0;
+        // while(i <s.length() && j< goal.length()){
+        //     if(s.charAt(i) != goal.charAt(j)){
+        //         i++;
+        //     }
+        //     else{
+        //         index=i;
+        //         while(i <s.length() && j< goal.length()){
+        //             if(s.charAt(i) != goal.charAt(j)){
+        //                 return false;
+        //             }
+        //             i++;
+        //             j++;
+        //         }
+        //     }
+        // }
+        // return true;
+        
     }
 }
