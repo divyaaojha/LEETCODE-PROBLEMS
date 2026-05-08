c8115c6d2a918169f6d945548052ee072b8bd7fa
Divyaa Ojha <divyaojha792@gmail.com>
2026-05-08 08:28:56 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (76.35%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,27 +10,55 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode (0); // put zero in it;
-        ListNode curr = dummy;
-        int carry =0; // for first digit sum carry =0
-
-        while(l1 != null || l2 != null || carry!=0){
-            //until we make count= 0, if it was more; 
-            //untill elemets left in any L1 or L2 , ,
-            // if anyone is null , we add 0 for them, cuz the other list still has nodes
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-            int sum = x+ y + carry;
-
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        int carry =0;
+        while(l1 != null || l2 != null || carry!=0) // true|| true|| flase = true;
+        {
+            int sum = carry;
+            if(l1 != null){
+                sum+= l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum+= l2.val;
+                l2 = l2.next;
+            }
+            temp.next = new ListNode(sum%10);
+            temp = temp.next;
             carry = sum/10;
-            // curr has an element, single node, now keep making new nodes next to current, and add sum remainder
-            curr.next = new ListNode( sum % 10);
-            curr = curr.next;
-            if(l1 != null) l1=l1.next;
-            if(l2 != null) l2=l2.next;
-
         }
 
         return dummy.next;
+
+        //doesnt work******** 32/1500
+        // StringBuilder sb1 = new StringBuilder();
+        // StringBuilder sb2 = new StringBuilder();
+        // if(l1.val ==0 && l2.val ==0)return l1;
+        // ListNode temp =new ListNode(-1);
+        // ListNode ans = temp;
+        // while(l1 != null){
+        //     sb1.append(l1.val);
+        //     l1 = l1.next;
+        // }
+        // sb1.reverse();
+        //  while(l2 != null){
+        //     sb2.append(l2.val);
+        //     l2 = l2.next;
+        // }
+        // sb2.reverse();
+        // int a = Integer.parseInt(sb1.toString());
+        // int b = Integer.parseInt(sb2.toString());
+        // a = a+b;
+        // while(a>0 ){
+        //     int rem = a%10;
+        //     a = a/10;
+            
+        //  ans.next = new ListNode(rem);
+        //  ans= ans.next;
+           
+        // }
+
+        // return temp.next;
     }
 }
