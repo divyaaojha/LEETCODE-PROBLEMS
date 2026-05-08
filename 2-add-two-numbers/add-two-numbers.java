46c4130b32924899c31b9f10011e7c154081a36b
Divyaa Ojha <divyaojha792@gmail.com>
2026-05-08 11:42:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (76.35%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,27 +10,92 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        int carry =0;
-        while(l1 != null || l2 != null || carry!=0) // true|| true|| flase = true;
-        {
+        // lists are already reversed, so carry will move in forward direction only
+        //aa new list has to be initiated, as after adding 2 numbers, the number of digits can increase
+
+        ListNode dummy = new ListNode(-1); // new ll initiated
+        ListNode temp = dummy; // reference to ll dummy
+
+        int carry =0; // inthe beginning no carry
+
+        //there are 2 heads that we will be traversing l1 and l2 both, also we will ad thier value to sum only if they exist
+        //so there wont be any problem, if one list is of size say 5, and other of size 3, loop will run, and only add values of list which isnt null,
+        // also if carry is not 0 , loop will continue to run, this prevents premature exit from loop in certain cases say 99 + 99 , here carry is 1, l1 and l3 will be null in 3rd iteration, but still there is a carry so, its importain to add thos carry !=0 cond in loop
+        //   || operator so that even if any cond true, loop will run
+        while(l1 != null || l2 != null || carry !=0 ){
             int sum = carry;
-            if(l1 != null){
-                sum+= l1.val;
-                l1 = l1.next;
+            //l1 node value added
+            if(l1!= null){
+                sum += l1.val;
+                l1= l1.next;
             }
-            if(l2 != null){
-                sum+= l2.val;
-                l2 = l2.next;
+            //now add l2 s node value
+            if(l2!= null){
+                sum += l2.val;
+                l2= l2.next;
             }
+            //we need to create a node at temp.next, and then we ll move ahead, so that we arent moving to null
             temp.next = new ListNode(sum%10);
             temp = temp.next;
-            carry = sum/10;
+            carry = sum /10;
+
         }
 
         return dummy.next;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ListNode dummy = new ListNode(-1);
+        // ListNode temp = dummy;
+        // int carry =0;
+        // while(l1 != null || l2 != null || carry!=0) // true|| true|| flase = true;
+        // {
+        //     int sum = carry;
+        //     if(l1 != null){
+        //         sum+= l1.val;
+        //         l1 = l1.next;
+        //     }
+        //     if(l2 != null){
+        //         sum+= l2.val;
+        //         l2 = l2.next;
+        //     }
+        //     temp.next = new ListNode(sum%10);
+        //     temp = temp.next;
+        //     carry = sum/10;
+        // }
+
+        // return dummy.next;
+//************************************************************************************************************ */
         //doesnt work******** 32/1500
         // StringBuilder sb1 = new StringBuilder();
         // StringBuilder sb2 = new StringBuilder();
