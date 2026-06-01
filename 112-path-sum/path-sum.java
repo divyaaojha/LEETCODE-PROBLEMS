768c49926705ed1ca66d878f945c4c9e15245ecb
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-01 17:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (48.27%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean pathsum (TreeNode node, int target){
+        if(node==null){
+            return false;
+        }
+        
+        if(node.left==null && node.right == null){
+            return target == node.val;  //return true or false
+        }
+        
+        return pathsum(node.left, target- node.val) || pathsum(node.right, target- node.val);
+
+
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        return pathsum(root, targetSum);
+    }
+}
