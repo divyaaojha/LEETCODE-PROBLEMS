77452525c939a54d3a897e57d542d816719529d7
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-09 09:33:26 +0530

    Time: 2 ms (91.78%) | Memory: 48 MB (6.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,121 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int left =0;
-         int i =1;
-         while(i<n){
-            if(nums[left]==0 & nums[i] != 0){
-                nums[left++] =nums[i];
-                nums[i]=0;
+        int i =0; int k =0;
+        while(i <n){
+            if(nums[i] !=  0){
+                nums[k++]=nums[i++];
             }
-            if(nums[left] !=0){
-                left ++;
+            else{
+                i++;
             }
-
-            i++;
-         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //in-place Solution
-        // int index =0;
-        // for(int i =0; i<nums.length; i++){
-        //     if(nums[i] !=0){
-
-        //         //swap isnt necessary when i and index are same , we swap only when element is not zero, and index and i are different
-        //         if(i != index){
-        //         // swap this to the index position as that has the latest zero
-        //         int temp = nums[index];
-        //         nums[index]= nums[i];
-        //         nums[i]= temp;
-                
-        //         }
-        //         index ++;
-                
-        //     }
-        // }
-
-//wont work, as they need it to be in-place
-//        int count=0;
-//        for(int i =0; i<nums.length; i++){
-//         if(nums[i] ==0){
-//             count++;
-//             nums[i]= Integer.MAX_VALUE;
-//         }
-//        } 
-//        if(count>0){
-//  Arrays.sort(nums);
-//        }
-      
-
-// for(int i =0; i<nums.length; i++){
-//         if(nums[i] ==Integer.MAX_VALUE){
-//             count++;
-//             nums[i]=0 ;
-//         }
-//        }
+        }
+        while(k<n){
+            nums[k++]=0;
+        }
     }
 }
