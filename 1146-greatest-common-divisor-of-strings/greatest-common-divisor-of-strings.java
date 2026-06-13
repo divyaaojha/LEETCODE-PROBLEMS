5a2e495fad57b546a108f3239df0adbb637ed88f
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-13 08:33:58 +0530

    Time: 2 ms (16.72%) | Memory: 46.4 MB (10.00%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,24 +1,4 @@
 class Solution {
-    public int gcd (int a, int b){
-        if (b == 0){
-            return a;
-        }
-        return gcd(b , a%b);
-    }
-    public String gcdOfStrings(String str1, String str2) {
-        if (!(str1 + str2).equals(str2 + str1)) {
-        return "";
-        }
-        int a = str1.length();
-        int b = str2.length();
-        int i = gcd(a,b);
-        return str1.substring(0, i);
-    }
-}
-
-
-/*
-class Solution {
     public boolean helper(String str, String pattern ){
         //helper function to divide and pattern building
         int n = str.length();
@@ -38,7 +18,6 @@ class Solution {
     public String gcdOfStrings(String str1, String str2) {
         int minlen = Math.min(str1.length(), str2.length());
 
-
         //starting division /finding from larger strings at the end, so that when match happens , we return and its largest match we returned
         for(int i =minlen; i>=1; i--){
             String candidate = str1.substring(0,i);
@@ -47,6 +26,27 @@ class Solution {
             }
         }
         return "";
+    }
+}
+
+
+
+/*
+class Solution {
+    public int gcd (int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b , a%b);
+    }
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+        return "";
+        }
+        int a = str1.length();
+        int b = str2.length();
+        int i = gcd(a,b);
+        return str1.substring(0, i);
     }
 }
 */
