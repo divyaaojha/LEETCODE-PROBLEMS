42d253cfeb1caa499676c22f811ddb6ec48f5d0d
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-15 11:48:57 +0530

    Time: 3 ms (96.92%) | Memory: 52.5 MB (6.80%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,71 +1,21 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-      int n = nums.length;
-        int maxcount=0;
-      for(int i =0; i< n ; i++){
+        int left =0;
         int zeros=0;
+        int maxlen=0;
+        for(int right =0; right<nums.length; right++){
 
-        for(int j =i; j<n; j++){
-            if(nums[j]==0){
+            if(nums[right]==0){
                 zeros++;
             }
-            if(zeros>k){
-                break;
+            while(zeros > k){
+                if(nums[left]==0){
+                    zeros--;
+                }
+                left++;
             }
-            maxcount= Math.max(maxcount, j-i+1);
+            maxlen= Math.max(maxlen, right-left+1);
         }
-      } 
-      return maxcount; 
+        return maxlen;
     }
 }
-
-
-/*
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int n = nums.length;
-        
-        int maxcount =0;
-       
-        for(int i =0; i< n ; i++){
-            int m =0;
-             int count=0;
-            if(nums[i] ==0 && m < k ){
-                m++;
-                count++;
-                
-            }
-            if(nums[i]==1){
-                count++;
-            }
-
-            for(int j =i+1; j<n; j++ ){
-                if(m<k){
-                    if(nums[j]==0){
-                        m++;
-                        count++;
-                        
-                    }
-                    else{
-                        count++;
-                    }
-                }
-                else if(nums[j] ==0){
-                   
-                    break;
-                }
-                else{
-                    count++;
-                }
-                
-                   
-            
-
-            }
-            maxcount= Math.max(maxcount, count);
-        }
-        return maxcount;
-    }
-}
-
-*/
