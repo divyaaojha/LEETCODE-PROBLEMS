8c3baaa970c301861f1722fad85047352a78e694
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-15 13:48:40 +0530

    Time: 1 ms (98.49%) | Memory: 47.5 MB (22.04%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int prefix[]= new int[n];
+        prefix[0]=0;
+        int suffix[]= new int[n];
+        suffix[n-1]=0;
+        int sum1=0;
+        int sum2=0;
+        //building prefi
+        for(int i =1; i<n; i++){
+            sum1 +=nums[i-1];
+            prefix[i]=sum1;
+        }
+        for(int i = n-2; i>=0; i--){
+            sum2 += nums[i+1];
+            suffix[i]= sum2;
+        }
+        
+        for(int i =0; i< n; i++ ){
+            if(prefix[i]==suffix[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
