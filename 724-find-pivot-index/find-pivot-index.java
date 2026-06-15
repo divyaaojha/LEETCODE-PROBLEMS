bbec4e19378cf95901cea819ef6bf126717634c3
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-15 14:15:48 +0530

    Time: 1 ms (98.49%) | Memory: 47.4 MB (39.65%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,27 +1,49 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int leftsum =0;
         int n = nums.length;
-        int prefix[]= new int[n];
-        prefix[0]=0;
-        int suffix[]= new int[n];
-        suffix[n-1]=0;
-        int sum1=0;
-        int sum2=0;
-        //building prefi
-        for(int i =1; i<n; i++){
-            sum1 +=nums[i-1];
-            prefix[i]=sum1;
+
+        int totalsum=0;
+        for(int num: nums){
+            totalsum+=num;
         }
-        for(int i = n-2; i>=0; i--){
-            sum2 += nums[i+1];
-            suffix[i]= sum2;
-        }
-        
         for(int i =0; i< n; i++ ){
-            if(prefix[i]==suffix[i]){
+             
+            int rightsum=totalsum-leftsum- nums[i];
+            if(leftsum==rightsum){
                 return i;
             }
+            leftsum+=nums[i];
+           
         }
+
+
         return -1;
+
+
+    // 
+        // int n = nums.length;
+        // int prefix[]= new int[n];
+        // prefix[0]=0;
+        // int suffix[]= new int[n];
+        // suffix[n-1]=0;
+        // int sum1=0;
+        // int sum2=0;
+        // //building prefi
+        // for(int i =1; i<n; i++){
+        //     sum1 +=nums[i-1];
+        //     prefix[i]=sum1;
+        // }
+        // for(int i = n-2; i>=0; i--){
+        //     sum2 += nums[i+1];
+        //     suffix[i]= sum2;
+        // }
+        
+        // for(int i =0; i< n; i++ ){
+        //     if(prefix[i]==suffix[i]){
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 }
