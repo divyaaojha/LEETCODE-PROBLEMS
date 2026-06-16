1b50d81c5980ed6baffd722e8587474259e018a2
Divyaa Ojha <divyaojha792@gmail.com>
2026-06-16 10:09:45 +0530

    Time: 12 ms (59.87%) | Memory: 47.8 MB (83.99%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,4 +1,34 @@
 class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int n1= word1.length();
+        int n2= word2.length();
+        if(word1.length()!= word2.length()){
+
+        }
+       int[]freq1= new int[26]; 
+       int[]freq2= new int[26];
+       for(char c : word1.toCharArray()){
+        freq1[c-'a']++;
+       }
+       for(char c : word2.toCharArray()){
+        freq2[c-'a']++;
+       }
+
+       for(int i=0; i<26; i++){
+        if((freq1[i]==0) != (freq2[i]==0)){
+            return false;
+        }
+       }
+       Arrays.sort(freq1);
+       Arrays.sort(freq2);
+       return Arrays.equals(freq1,  freq2);
+
+
+    }
+}
+
+/*
+class Solution {
     boolean check1(String s1, String s2){
         HashMap<Character, Integer> map1= new HashMap<>();
         HashMap<Character, Integer> map2= new HashMap<>();
@@ -81,3 +111,4 @@ class Solution {
 
     }
 }
+*/
